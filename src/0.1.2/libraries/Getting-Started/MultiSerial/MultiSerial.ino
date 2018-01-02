ea2204724f463c8534c977889743071aee92bfa3
oferzvik <33871453+oferzvik@users.noreply.github.com>
2018-01-02 14:07:58 +0200

    bug fix

--- src/0.1.2/libraries/Getting-Started/MultiSerial/MultiSerial.ino
+++ src/0.1.2/libraries/Getting-Started/MultiSerial/MultiSerial.ino
@@ -24,19 +24,19 @@
 void setup() {
   // initialize both serial ports:
   Serial.begin(115200);
-  Serial2.begin(115200);
+  Serial1.begin(115200);
 }
 
 void loop() {
   // read from port 1, send to port 0:
-  if (Serial2.available()) {
-    int inByte = Serial2.read();
+  if (Serial1.available()) {
+    int inByte = Serial1.read();
     Serial.write(inByte);
   }
 
   // read from port 0, send to port 1:
   if (Serial.available()) {
     int inByte = Serial.read();
-    Serial2.write(inByte);
+    Serial1.write(inByte);
   }
 }
